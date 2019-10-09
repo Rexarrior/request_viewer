ab3c402680a39245c1124c405c0953b9d0a6ad83
Ksandr <Rexarrior@yandex.ru>
2019-10-09 14:45:16 +0300

    Add files via upload

--- web-service/manage.py
+++ web-service/manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'judyst_web_service.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_service.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- web-service/web_service/wsgi.py
+++ web-service/web_service/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for judyst_web_service project.
+
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
